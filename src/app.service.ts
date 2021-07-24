e02de754b1daf225a21e2fa83654289a28f14112
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-07-24 11:54:54 +0000

    feat: update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as dayjs from 'dayjs';
+import * as isBetween from 'dayjs/plugin/isBetween';
 import * as timezone from 'dayjs/plugin/timezone';
 import * as utc from 'dayjs/plugin/utc';
 dayjs.extend(utc)
+dayjs.extend(isBetween)
 dayjs.extend(timezone)
 dayjs.tz.setDefault("Asia/Bangkok");
 
@@ -17,6 +19,14 @@ export class AppService {
     try {
       const now = dayjs().tz();
       console.log(now);
+
+      const date1 = dayjs('2021-08-02');
+      const date2 = dayjs('2021-07-31');
+      const result = date1.diff(date2);
+      console.log(result);
+
+      const isBetween =  dayjs('2021-07-01').isBetween('2021-07-01', '2021-07-31');
+      console.log(isBetween);
     } catch (error) {
       console.log(error);
     }
