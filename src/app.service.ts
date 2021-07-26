036e61eb01067cd23a68674bebde324d53c737fe
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-07-26 15:05:04 +0700

    feat: update function

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
 
   constructor(private readonly timeService: TimeService) {
     this.onStart();
+    // this.getDatesRange('2021-12-31T02:30:00.000Z' , '2022-01-01T17:00:00.000Z')
   }
 
   public async onStart() {
@@ -17,15 +18,15 @@ export class AppService {
     }
   }
 
-  // public async getDatesRange(start: Date, stop: Date): Promise<string[]> {
-  //   const dateArray = [];
-  //   let currentDate = dayjs(start).tz().format('YYYY-MM-DD');
-  //   let stopDate = dayjs(stop).tz().format('YYYY-MM-DD');
-  //   while (currentDate <= stopDate) {
-  //     dateArray.push(dayjs(currentDate).tz().format('YYYY-MM-DD'))
-  //     currentDate = dayjs(currentDate).add(1, 'days').format('YYYY-MM-DD');
-  //   }
-  //   console.log(dateArray)
-  //   return dateArray;
-  // }
+  public async getDatesRange(start: string, stop: string): Promise<string[]> {
+    const dateArray = [];
+    let currentDate = this.timeService.dayjs(start).tz().format('YYYY-MM-DD');
+    let stopDate = this.timeService.dayjs(stop).tz().format('YYYY-MM-DD');
+    while (currentDate <= stopDate) {
+      dateArray.push(this.timeService.dayjs(currentDate).tz().format('YYYY-MM-DD'))
+      currentDate = this.timeService.dayjs(currentDate).add(1, 'days').format('YYYY-MM-DD');
+    }
+    console.log(dateArray)
+    return dateArray;
+  }
 }
