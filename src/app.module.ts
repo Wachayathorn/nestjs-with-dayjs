e80bfbbc4bc0695919bcf11c6b6cbb9776954ee1
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2021-07-30 10:07:53 +0700

    feat: add metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
+import { PromModule } from '@digikare/nestjs-prom';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TimeService } from './time/time.service';
 
 @Module({
-  imports: [TimeService],
+  imports: [PromModule.forRoot({
+    defaultLabels: {
+      app: 'app',
+      version: '1.0.0',
+    }
+  }),
+    TimeService],
   controllers: [AppController],
-  providers: [AppService , TimeService],
+  providers: [AppService, TimeService],
 })
-export class AppModule {}
+export class AppModule { }
